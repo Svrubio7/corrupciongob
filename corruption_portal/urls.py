464320a01e445196411d4eb221c76141b6509936
Svrubio7 <sverdugo.ieu2022@student.ie.edu>
2025-07-11 18:57:44 +0200

    url patterns2

--- corruption_portal/urls.py
+++ corruption_portal/urls.py
@@ -25,10 +25,7 @@ urlpatterns = [
     path('api/', include('corruption_cases.urls')),
     re_path(r'^(?!static/|media/|admin/|api/).*$', TemplateView.as_view(template_name="index.html")),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# Serve static/media in development (optional, but helpful)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
