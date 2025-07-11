2e7ac683f7d2672f7b867231ec17d6f8f5d88610
Svrubio7 <sverdugo.ieu2022@student.ie.edu>
2025-07-11 18:13:40 +0200

    urls

--- corruption_portal/urls.py
+++ corruption_portal/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = ['',
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('corruption_cases.urls')),
     re_path(r'^(?!static/|media/|admin/|api/).*$', TemplateView.as_view(template_name="index.html")),
