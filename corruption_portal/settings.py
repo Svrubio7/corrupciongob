0e74281ab4ca56056866291437ac6445a5eed556
Svrubio7 <sverdugo.ieu2022@student.ie.edu>
2025-07-18 14:38:29 +0200

    corrected settings for page_size = 30

--- corruption_portal/settings.py
+++ corruption_portal/settings.py
@@ -149,7 +149,7 @@ CSRF_TRUSTED_ORIGINS = [
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 12,
+    'PAGE_SIZE': 30,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
